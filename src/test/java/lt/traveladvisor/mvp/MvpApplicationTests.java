97554c5be75f72b7a1934ac33c3aa263df69a460
Treciokas <to.treciokas@gmail.com>
2021-05-07 17:17:22 +0300

    Initial commit

---
+++ src/test/java/lt/traveladvisor/mvp/MvpApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.traveladvisor.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
